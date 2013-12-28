3d0cdc4cfb4c05514b40ac50d932409617e1f18c
Shasha <eileenss8824@gmail.com>
2013-12-28 16:54:14 +0800

    [shasha] add route for server .

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var http = require("http")
+var url = require("url")
 
-function start(){
-	http.createServer(function(request, response){
-	console.log("request received");
-	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.write("Hello world");
-	response.end();
-	}).listen(2024);
+function start(route,handle){
+	function onRequest(request, response){
+		console.log("request received");
+		var pathname = url.parse(request.url).pathname;
+		console.log("request for "+pathname+" received");
+
+		route(pathname,handle);
+
+		response.writeHead(200,{"Content-Type":"text/plain"});
+		response.write("Hello world");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(2024);
 
 	console.log("Server Started");	
 }
