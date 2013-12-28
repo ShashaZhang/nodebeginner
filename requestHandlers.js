8b8758b63eb867df78deaddf638bc0a4e020d136
Shasha <eileenss8824@gmail.com>
2013-12-28 16:59:06 +0800

    [shasha] display message according to requesthandler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start () {
 	console.log("Request handler 'start' was called. ");
+	return "Hello Start";
 }
 
 function upload(){
 	console.log("Request hadler 'upload' was called. ");
+	return "404 Error";
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -7,10 +7,10 @@ function start(route,handle){
 		var pathname = url.parse(request.url).pathname;
 		console.log("request for "+pathname+" received");
 
-		route(pathname,handle);
+		var result = route(pathname,handle);
 
 		response.writeHead(200,{"Content-Type":"text/plain"});
-		response.write("Hello world");
+		response.write(result);
 		response.end();
 	}
 
